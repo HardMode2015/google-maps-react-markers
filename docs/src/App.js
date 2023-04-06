e68d8b1675fb7683d644919008be7377a5dd9bcb
Foo <twistleech@gmail.com>
2023-04-06 15:57:50 +0200

    react 17 and 18

--- docs/src/App.js
+++ docs/src/App.js
@@ -66,7 +66,7 @@ const App = () => {
 		setHighlighted(markerId)
 	}
 
-	const onMapChange = ({ bounds, zoom, marginBounds }) => {
+	const onMapChange = ({ bounds, zoom }) => {
 		const ne = bounds.getNorthEast()
 		const sw = bounds.getSouthWest()
 		/**
